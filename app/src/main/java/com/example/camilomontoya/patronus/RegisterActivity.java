61185178b3571286d90bfec3b7c34f23dd57884e
korneas <cajomo0311@gmail.com>
2017-11-13 18:57:50 -0500

    Update register

---
+++ app/src/main/java/com/example/camilomontoya/patronus/RegisterActivity.java
@@ -0,0 +1,114 @@
+package com.example.camilomontoya.patronus;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+
+    private EditText name, email, residence, pass, passConfirm;
+    private Button btnReg;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        getSupportActionBar().setTitle("Registro");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        email = (EditText) findViewById(R.id.email_reg);
+        pass = (EditText) findViewById(R.id.pass_reg);
+
+        btnReg = (Button) findViewById(R.id.btn_reg);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String emailReg = email.getText().toString();
+                String passReg = pass.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(emailReg, passReg)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    aviso("No se pudo registrar usuario");
+                                } else {
+                                    aviso(emailReg + " registrado con exito!");
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    private void aviso (String txt) {
+        Toast.makeText(getApplicationContext(), txt, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/com/example/camilomontoya/patronus/StartActivity.java
+++ app/src/main/java/com/example/camilomontoya/patronus/StartActivity.java
@@ -1,22 +1,34 @@
 package com.example.camilomontoya.patronus;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class StartActivity extends AppCompatActivity {
+
+    private static final String TAG = "StartActivity";
 
     private EditText email,pass;
     private Button btnLogin;
     private TextView register;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +36,15 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
         getSupportActionBar().hide();
 
+        mAuth = FirebaseAuth.getInstance();
+
         Typo.getInstance().setTitle(Typeface.createFromAsset(getAssets(),"fonts/Quicksand-Bold.ttf"));
         Typo.getInstance().setSpecial(Typeface.createFromAsset(getAssets(),"fonts/Raleway-BoldItalic.ttf"));
         Typo.getInstance().setContent(Typeface.createFromAsset(getAssets(),"fonts/Quicksand-Regular.ttf"));
 
-        email = (EditText) findViewById(R.id.email_login);
-        pass = (EditText) findViewById(R.id.pass_login);
-        btnLogin = (Button) findViewById(R.id.btn_login);
+        email = (EditText) findViewById(R.id.email_reg);
+        pass = (EditText) findViewById(R.id.pass_reg);
+        btnLogin = (Button) findViewById(R.id.btn_reg);
         register = (TextView) findViewById(R.id.register_txt);
 
         email.setTypeface(Typo.getInstance().getContent());
@@ -44,5 +58,55 @@ public class StartActivity extends AppCompatActivity {
         str.setSpan(new ForegroundColorSpan(Color.rgb(236,110,173)), inicioTexto.length(), inicioTexto.length()+negrilla.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         str.setSpan(new StyleSpan(Typeface.BOLD_ITALIC), inicioTexto.length(), inicioTexto.length()+negrilla.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         register.setText(str);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,RegisterActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailAuth = email.getText().toString();
+                String passAuth = pass.getText().toString();
+
+                if(!emailAuth.equals("") && !passAuth.equals("")){
+                    mAuth.signInWithEmailAndPassword(emailAuth, passAuth);
+                } else {
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
